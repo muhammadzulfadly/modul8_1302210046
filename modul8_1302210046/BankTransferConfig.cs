4c5a1ae528c56c9ff986ae1ef9f4166888c4a3fe
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2023-04-11 17:18:00 +0700

    aplikasi bank trasfer dengan runtime configuration

---
+++ modul8_1302210046/BankTransferConfig.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace modul8_1302210046
+{
+    public class BankTransferConfig
+    {
+        public Bank bank { get; set; }
+        public string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public string configFileName = "bank_transfer_config.json";
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                ReadConfig();
+            }
+            catch
+            {
+                SetDefault();
+                WriteConfig();
+            }
+        }
+
+        private Bank ReadConfig()
+        {
+            string jsonFromFile = File.ReadAllText(path + '/' + configFileName);
+            bank = JsonSerializer.Deserialize<Bank>(jsonFromFile);
+            return bank;
+        }
+
+        private void WriteConfig()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String jsonString = JsonSerializer.Serialize(bank, options);
+            string fullPath = path + '/' + configFileName;
+            File.WriteAllText(fullPath, jsonString);
+        }
+
+        private void SetDefault()
+        {
+            bank = new Bank("en", 25000000, 6500, 15000, ["RTO(real-time)", "SKN", "RTGS", "BI FAST"], "yes", "ya");
+        }
+
+
+        public class Bank
+        {
+            public string lang { get; set; }
+            public Transfer transfer { get; set; }
+            public string method { get; set; }
+            public Confirmation confirmation { get; set; }
+            public Bank(string lang, Transfer transfer, string method, Confirmation confirmation)
+            {
+                this.lang = lang;
+                this.transfer = transfer;
+                this.method = method;
+                this.confirmation = confirmation;
+            }
+         }
+
+        public class Transfer
+        {
+            public int en { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+            public Transfer(int en, int low_fee, int high_fee)
+            {
+                this.en = en;
+                this.low_fee = low_fee;
+                this.high_fee = high_fee;
+            }
+        }
+
+        public class Confirmation
+        {
+            public string en { get; set; }
+            public string id { get; set; }
+            public Confirmation(string en, string id)
+            {
+                this.en = en;
+                this.id = id;
+            }
+        }
+    }
+}
--- modul8_1302210046/Program.cs
+++ modul8_1302210046/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace modul8_1302210046
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            BankTransferConfig bank = new BankTransferConfig();
+            Console.WriteLine(bank.bank = "en" ? "Please insert the amount of money to transfer:" : "Masukkan jumlah uang yang akan di-transfer:");
+            int amount = int.Parse(Console.ReadLine());
 
+            int transferfee1 = amount <= bank.transfer.low_fee;
+            int transferfee2 = amount >= bank.transfer.high_fee;
+
+            Console.WriteLine(bank.bank == "en" ? "Select transfer method:" : "Pilih metode transfer:")
+
+            if (confirmation == bank.Confirmation.en || confirmation == bank.Confirmation.id)
+            {
+                Console.WriteLine(bank.bank == "en" ? "The transfer is completed" : "Proses transfer berhasil");
+            }
+            else {
+                Console.WriteLine(bank.bank == "en" ? "Transfer is cancelled" : "Transfer dibatalkan");
+            }
+        }
+    }
+}
